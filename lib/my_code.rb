1c7f21adb27345afe6858a281e0d32dcc3ca19b8
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 16:01:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 # My Code here....
 
 def map_to_negativize(arr)
-  prc ||= {|ele| ele * -1}
+
   x = 0
   a = []
   while x < arr.length 
-   a << prc.call(arr[x])
+   a << arr[x] * -1 
    x += 1 
   end
   a 
